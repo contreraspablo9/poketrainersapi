aae007dcebe022949887bbdb562400d09484931a
contreraspablo9 <contreraspablo197@protonmail.com>
2022-01-17 10:22:34 -0600

    v1.10.0_apiStructure_addPut&DeleteMethods,bugFixes

--- api/functions.py
+++ api/functions.py
@@ -21,3 +21,28 @@ def delete(model_str, model, response, serializer, status, pk=None):
     instance.delete()
     instance = serializer(instance).data
     return response({'values':instance}, status=status.HTTP_202_ACCEPTED)
+
+def put(instance_str, response, status, rest_serializer, model, request, pk):
+    def add_params(make_changes): 
+        def inner(): 
+
+            if pk is None: 
+                return response(
+                    {'details':f'Error, no {instance_str} id was provided. Please, add it at the end of the URL'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            serializer = rest_serializer(data=request.data)
+            if serializer.is_valid(): 
+                try: 
+                    instance = model.objects.get(pk=pk)
+                except model.DoesNotExist: 
+                    return response(status=status.HTTP_404_NOT_FOUND)
+                instance = make_changes(instance, serializer.data, rest_serializer)
+                
+                return response(
+                    {'values':instance}, 
+                    status=status.HTTP_202_ACCEPTED
+                )
+            return response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return inner
+    return add_params
--- api/views.py
+++ api/views.py
@@ -66,9 +66,17 @@ class Trainers(APIView, PageNumberPagination):
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk=None, alias=None): 
-        print(request)
-        return Response()
+    def put(self, request, pk=None, **kwargs): 
+        @fn.put('trainer', Response, status, serializers.TrainerSerializer, models.TrainersDataT, request, pk)
+        def modify_trainer(trainer, data, serializer): 
+            trainer.name = data['name']
+            trainer.alias = data['alias']
+            trainer.age = data['age']
+            trainer.save()
+            trainer = serializer(trainer).data
+            return trainer
+
+        return modify_trainer()
    
     def delete  (self, request, pk=None, **kwargs):
         return fn.delete('trainer', models.TrainersDataT, Response, serializers.TrainerSerializer, status, pk)
@@ -119,8 +127,16 @@ class Teams(APIView, PageNumberPagination):
         else: 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def put(self, request): 
-        return Response()
+    def put(self, request, pk=None, **kwargs): 
+        @fn.put('team', Response, status, serializers.PostTeamSerializer, models.TeamsDataT, request, pk)
+        def modify_team(team, data, serializer): 
+            team.name = data['name']
+            team.trainer_id = data['trainer_id']
+            team.save()
+            team = serializer(team).data
+            return team
+
+        return modify_team()
 
     def delete  (self, request, pk=None, **kwargs):
         return fn.delete('team', models.TeamsDataT, Response, serializers.TeamSerializer, status, pk)
@@ -160,7 +176,13 @@ class TeamMembers(APIView,PageNumberPagination):
     def post(self, request, **kwargs): 
         serializer = serializers.PostTeamMemberSerializer(data=request.data)
         if serializer.is_valid(): 
-            team_instance = models.TeamsDataT.objects.get(pk=serializer.data['team_id'])
+            try: 
+                team_instance = models.TeamsDataT.objects.get(pk=serializer.data['team_id'])
+            except models.TeamsDataT.DoesNotExist:
+                return Response({'details':f"Error, the team with id {serializer.data['team_id']} does not exist"},status=status.HTTP_404_NOT_FOUND)
+            pokemon_data = fn.apicall(self.pokeapi_url + '?limit=3000')['results']
+            if serializer.data['pokemon_id'] > len(pokemon_data) - 1:
+                return Response({'details':f"Error, pokemon with id {serializer.data['pokemon_id']} does not exist"},status=status.HTTP_404_NOT_FOUND)
 
             team_members = models.TeamMembersT.objects.filter(teams=team_instance).count()
             if team_members >= 6:
@@ -170,8 +192,7 @@ class TeamMembers(APIView,PageNumberPagination):
                 teams=team_instance
             )
             new_member.save()
-            pokemon_data = fn.apicall(self.pokeapi_url + str(new_member.pokemon_id))
-            new_member.name = pokemon_data['name']
+            new_member.name = pokemon_data[new_member.pokemon_id - 1]['name']
             new_member.url = self.pokeapi_url + str(new_member.pokemon_id)
 
             new_member = serializers.TeamMemberSerializer(new_member).data
@@ -182,8 +203,16 @@ class TeamMembers(APIView,PageNumberPagination):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request): 
-        return Response()
+    def put(self, request, pk=None, **kwargs): 
+        @fn.put('member', Response, status, serializers.PostTeamMemberSerializer, models.TeamMembersT, request, pk)
+        def modify_team(member, data, serializer): 
+            member.pokemon_id = data['pokemon_id']
+            member.team_id = data['team_id']
+            member.save()
+            member = serializer(member).data
+            return member
+
+        return modify_team()
 
     def delete  (self, request, pk=None, **kwargs):
         return fn.delete('member', models.TeamMembersT, Response, serializers.TeamMemberSerializer, status, pk)
